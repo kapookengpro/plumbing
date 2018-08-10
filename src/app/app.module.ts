d4ca7a9ffc421f4631e00aa76330a336dc0e54c2
keng-at-635067465706 <kapookengpro@gmail.com>
2018-08-11 01:59:32 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { Demo1Component } from './main-page/demo1/demo1.component';
+import { Demo2Component } from './main-page/demo2/demo2.component';
+import { Demo3Component } from './main-page/demo3/demo3.component';
+
+import { AgmCoreModule } from '@agm/core';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    Demo1Component,
+    Demo2Component,
+    Demo3Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: ''
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
